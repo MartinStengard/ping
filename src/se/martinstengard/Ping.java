ed101cb4ad7c9822222db2e71d3537b451c4e34e
martinstengard <martin.stengard@gmail.com>
2018-02-07 22:41:53 +0100

    Change to one test only that will return ip lookup and url (GET) for http and https protocols.

--- src/se/martinstengard/Ping.java
+++ src/se/martinstengard/Ping.java
@@ -1,76 +1,63 @@
 
 package se.martinstengard;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ping {
-    /**
-     * PingResponse contain result of called method.
-     * This class will always be returned from all calls.
-     */
-    public class PingResponse{
+
+    private class PingResponse{
         public String dns = "";
         public String ip = "";
-        public String message = "";
-        /**
-         * success is true if supplied ip or dns address is reachable.
-         */
-        public Boolean succees = false;
-        public Boolean hasError = false;
+        public Boolean isReachable = false;
         public String errorMessage = "";
         
         @Override
         public String toString(){
             return "Dns: " + dns + "\n" +
             "Ip: " + ip + "\n" +
-            "Message: " + message + "\n" +
-            "Success: " + succees + "\n" +
-            "Has Error: " + hasError + "\n" +
+            "Is Reachable: " + isReachable + "\n" + 
             "Error Message: " + errorMessage;
         }
 
     }
     
-    public class UrlResponse{
-        public Integer code = 0;
-        public String message = "";
-        public Boolean hasError = false;
+    private class UrlResponse{
+        public String url = "";
+        public Integer httpStatusCode = 0;
+        public String httpMessage = "";
         public String errorMessage = "";
         
         @Override
         public String toString(){
-            return "Code: " + code + "\n" +
-            "Message: " + message + "\n" +
-            "Error Message: " + errorMessage;
+            return "Url: " + url + "\n" +
+                    "Http Status Code: " + httpStatusCode + "\n" +
+                    "Http Message: " + httpMessage + "\n" +
+                    "Error Message: " + errorMessage;
         }
     }
     
-    /**
-     * Check if ip address is reachable. 
-     * 
-     * @param ipAddress ip address to test; for example 123.123.123.123.
-     * @return PingResponse 
-     */
-    public PingResponse ip(String ipAddress){
-        return testIpOrDns(ipAddress);
+    public class Response{
+        public PingResponse pingResponse;
+        public List<UrlResponse> urlResponses = new ArrayList<UrlResponse>();
+        
+        @Override
+        public String toString(){
+            String urlString = "";
+            
+            for(UrlResponse response : urlResponses){
+                urlString += response.toString() + "\n\n";
+            }
+            
+            return pingResponse.toString() + "\n\n" + urlString;
+        }
     }
-
-    /**
-     * Check if dns address is reachable. 
-     * 
-     * @param dnsAddress dns name to test, without protocol; for example java.sun.com.
-     * @return PingResponse 
-     */
-    public PingResponse dns(String dnsAddress){
-        return testIpOrDns(dnsAddress);
-    }
-   
+  
     /**
      * Check if ip or dns address is reachable. 
-     * Will always return a PingResponse object.
      * 
      * @param ipOrDns dns name to test, without protocol; for example java.sun.com.
      *                ip address to test; for example 123.123.123.123.
@@ -84,57 +71,75 @@ public class Ping {
 
             if(inet == null){
                 pingResponse.errorMessage = ipOrDns + " could not be looked up.";
-                pingResponse.hasError = true;
                 return pingResponse;
             }
 
             pingResponse.dns = inet.getHostName();
             pingResponse.ip = inet.getHostAddress();
             
-            if (inet.isReachable(5000)){
-                pingResponse.message = ipOrDns + " is reachable.";
-                pingResponse.succees = true;
-            } else {
-                pingResponse.message = ipOrDns + " is NOT reachable.";
+            if (inet.isReachable(1000)){
+                pingResponse.isReachable = true;
             }
 
             return pingResponse;
             
         } catch (Exception e ) {
             pingResponse.errorMessage = "Unhandled error: " + e.getMessage();
-            pingResponse.hasError = true;
             return pingResponse;
         }
     }
 
     /**
      * Performs a GET request for supplied url.
-     * Will always return a UrlResponse object.
+     * 
      * @param url web url to test; for example https://www.oracle.com/index.html.
-     * @return UrlResponse
+     * @return List<UrlResponse> list with response for http and https.
      */
-    public UrlResponse url(String url){
-        UrlResponse urlResponse = new UrlResponse();
+    private List<UrlResponse> testUrl(String url){
+        List<UrlResponse> list = new ArrayList<UrlResponse>();
         
-        try {
-            URL siteURL = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
+        for (String aString : new String[] {"http://", "https://"}) {
+            UrlResponse response = new UrlResponse();
 
-            urlResponse.code = connection.getResponseCode();
-            urlResponse.message = connection.getResponseMessage();
-            
-            return urlResponse;
+            try {
+                URL siteURL = new URL(aString + url);
+                HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
 
-        } catch (Exception e) {
-            urlResponse.hasError = true;
-            urlResponse.errorMessage = e.getMessage();
-            return urlResponse;
+                response.url = aString + url;
+                response.httpStatusCode = connection.getResponseCode();
+                response.httpMessage = connection.getResponseMessage();
+
+            } catch (Exception e) {
+                response.errorMessage = e.getMessage();
+            }
+
+            list.add(response);
         }
+        
+        return list;
     }
     
-    
+    /**
+     * Test ping and return ip, http and https responses for supplied dns.
+     * @param dns dns name to test, without protocol; for example java.sun.com.
+     * @return Response Contain ip, http and https responses.
+     */
+    public Response test(String dns){
+        // 1. Test as dns or ip.
+        PingResponse pingResponse = testIpOrDns(dns);
+        
+        // 2. Test as url.
+        List<UrlResponse> urlResponses = testUrl(dns);
+        
+        // 3. Add to one resultset.
+        Response response = new Response();
+        response.pingResponse = pingResponse;
+        response.urlResponses = urlResponses;
+        
+        return response;
+    }
 }
 
  
