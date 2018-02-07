29eda669baf0f91eb266b981c4b371b13bb8f294
Martin Stengård <martin.stengard@softhouse.se>
2018-02-07 12:26:27 +0100

    Renaming.

--- src/se/martin/ci/stengard/Ping.java
+++ src/se/martin/ci/stengard/Ping.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package se.martin.ci.stengard;
 
@@ -11,39 +6,134 @@ import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
 
-/**
- *
- * @author mst
- */
-public class ping {
-    
-    public static String ip(String ip){
-      try {
-        InetAddress inet = InetAddress.getByName(ip);
-
-        if (inet.isReachable(5000)){
-          return ip + " is reachable.";
-        } else {
-          return ip + " NOT reachable.";
+public class Ping {
+    /**
+     * PingResponse contain result of called method.
+     * This class will always be returned from all calls.
+     */
+    public class PingResponse{
+        public String dns = "";
+        public String ip = "";
+        public String message = "";
+        /**
+         * success is true if supplied ip or dns address is reachable.
+         */
+        public Boolean succees = false;
+        public Boolean hasError = false;
+        public String errorMessage = "";
+        
+        @Override
+        public String toString(){
+            return "Dns: " + dns + "\n" +
+            "Ip: " + ip + "\n" +
+            "Message: " + message + "\n" +
+            "Success: " + succees + "\n" +
+            "Has Error: " + hasError + "\n" +
+            "Error Message: " + errorMessage;
         }
-      } catch ( IOException e ) {
-        return "Exception: " + e.getMessage();
-      }
+
     }
     
-    public static String url(String url){
+    public class UrlResponse{
+        public Integer code = 0;
+        public String message = "";
+        public Boolean hasError = false;
+        public String errorMessage = "";
+        
+        @Override
+        public String toString(){
+            return "Code: " + code + "\n" +
+            "Message: " + message + "\n" +
+            "Error Message: " + errorMessage;
+        }
+    }
+    
+    /**
+     * Check if ip address is reachable. 
+     * 
+     * @param ipAddress ip address to test; for example 123.123.123.123.
+     * @return PingResponse 
+     */
+    public PingResponse ip(String ipAddress){
+        return testIpOrDns(ipAddress);
+    }
+
+    /**
+     * Check if dns address is reachable. 
+     * 
+     * @param dnsAddress dns name to test, without protocol; for example java.sun.com.
+     * @return PingResponse 
+     */
+    public PingResponse dns(String dnsAddress){
+        return testIpOrDns(dnsAddress);
+    }
+   
+    /**
+     * Check if ip or dns address is reachable. 
+     * Will always return a PingResponse object.
+     * 
+     * @param ipOrDns dns name to test, without protocol; for example java.sun.com.
+     *                ip address to test; for example 123.123.123.123.
+     * @return PingResponse 
+     */
+    private PingResponse testIpOrDns(String ipOrDns){
+        PingResponse pingResponse = new PingResponse();
+
+        try {
+            InetAddress inet = InetAddress.getByName(ipOrDns);
+
+            if(inet == null){
+                pingResponse.errorMessage = ipOrDns + " could not be looked up.";
+                pingResponse.hasError = true;
+                return pingResponse;
+            }
+
+            pingResponse.dns = inet.getHostName();
+            pingResponse.ip = inet.getHostAddress();
+            
+            if (inet.isReachable(5000)){
+                pingResponse.message = ipOrDns + " is reachable.";
+                pingResponse.succees = true;
+            } else {
+                pingResponse.message = ipOrDns + " is NOT reachable.";
+            }
+
+            return pingResponse;
+            
+        } catch (Exception e ) {
+            pingResponse.errorMessage = "Unhandled error: " + e.getMessage();
+            pingResponse.hasError = true;
+            return pingResponse;
+        }
+    }
+
+    /**
+     * Performs a GET request for supplied url.
+     * Will always return a UrlResponse object.
+     * @param url web url to test; for example https://www.oracle.com/index.html.
+     * @return UrlResponse
+     */
+    public UrlResponse url(String url){
+        UrlResponse urlResponse = new UrlResponse();
+        
         try {
             URL siteURL = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
 
-            return connection.getResponseCode() + " " + connection.getResponseMessage();
+            urlResponse.code = connection.getResponseCode();
+            urlResponse.message = connection.getResponseMessage();
+            
+            return urlResponse;
 
-        } catch (IOException e) {
-            return "Exception: " + e.getMessage();
+        } catch (Exception e) {
+            urlResponse.hasError = true;
+            urlResponse.errorMessage = e.getMessage();
+            return urlResponse;
         }
     }
+    
     
 }
 
